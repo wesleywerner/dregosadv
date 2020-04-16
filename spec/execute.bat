6f2b5f0bc270be07c198df5abbd58eacfb8f2220
Wesley Werner <wesley.werner@gmail.com>
2020-04-17 07:17:05 +1000

    Fix tests to run direct from shell.

--- spec/execute.bat
+++ spec/execute.bat
@@ -1,3 +1,5 @@
+@call w:\coding\i6\setvars.bat
+
 ::REM Run Tests
 if exist catch_fish.actual del catch_fish.actual
 cat catch_fish.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > catch_fish.actual
@@ -9,3 +11,5 @@ echo [ Catch Fish --------------------------------------------------------] >> t
 diff --ignore-space-change catch_fish.expected catch_fish.actual >> test_results.txt
 echo [ Compass -----------------------------------------------------------] >> test_results.txt
 diff --ignore-space-change compass.expected compass.actual >> test_results.txt
+
+pause
