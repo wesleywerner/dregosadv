5f18403d52c8ad8cd2707e2b76a468341f4520c2
Wesley Werner <wesley.werner@gmail.com>
2020-04-19 11:53:50 +1000

    Add movement tests.

--- spec/execute.bat
+++ spec/execute.bat
@@ -4,6 +4,7 @@
 if exist catch_fish.actual del catch_fish.actual
 cat catch_fish.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > catch_fish.actual
 cat compass.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > compass.actual
+cat movement.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > movement.actual
 
 ::REM Diff Results
 if exist test_results.txt del test_results.txt
@@ -11,5 +12,7 @@ echo [ Catch Fish --------------------------------------------------------] >> t
 diff --ignore-space-change catch_fish.expected catch_fish.actual >> test_results.txt
 echo [ Compass -----------------------------------------------------------] >> test_results.txt
 diff --ignore-space-change compass.expected compass.actual >> test_results.txt
+echo [ Movement ----------------------------------------------------------] >> test_results.txt
+diff --ignore-space-change movement.expected movement.actual >> test_results.txt
 
 pause
---
+++ spec/movement.spec
@@ -0,0 +1,16 @@
+brief
+go faster
+exit
+north
+west
+go up
+go down
+climb tree
+climb down tree
+climb down
+go northeast
+go up
+go down
+climb stairs
+cross bridge
+jump over waterfall
