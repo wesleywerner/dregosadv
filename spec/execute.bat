46d02d2e2837bf1cdb80eb8d0b143ae966b43676
Wesley Werner <wesley.werner@gmail.com>
2020-04-24 07:29:34 +1000

    Output test results to console after execute.

--- spec/execute.bat
+++ spec/execute.bat
@@ -1,11 +1,11 @@
+@echo off
 @call w:\coding\i6\setvars.bat
 
 ::REM Run Tests
-if exist catch_fish.actual del catch_fish.actual
 cat catch_fish.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > catch_fish.actual
 cat compass.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > compass.actual
 cat movement.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > movement.actual
-cat garden.spec c | dfrotz -s 0 -p ..\DrEgoAdv.z5 > garden.actual
+cat garden.spec | dfrotz -s 0 -p ..\DrEgoAdv.z5 > garden.actual
 
 ::REM Diff Results
 if exist test_results.txt del test_results.txt
@@ -17,3 +17,6 @@ echo [ Movement ----------------------------------------------------------] >> t
 diff --ignore-space-change movement.expected movement.actual >> test_results.txt
 echo [ Garden ------------------------------------------------------------] >> test_results.txt
 diff --ignore-space-change garden.expected garden.actual >> test_results.txt
+
+::REM Output Results
+cat test_results.txt
