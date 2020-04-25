4c4bb297e1509add0ae2e7766e2706a3392531ef
Wesley Werner <wesley.werner@gmail.com>
2020-04-25 14:00:12 +1000

    Swap diff merge parameters to read left-to-right.

--- spec/merge.bat
+++ spec/merge.bat
@@ -1,7 +1,7 @@
 @echo off
 @call w:\coding\i6\setvars.bat
-kdiff3 catch_fish.actual   catch_fish.expected   -o catch_fish.expected
-kdiff3 compass.actual      compass.expected      -o compass.expected
-kdiff3 garden.actual       garden.expected       -o garden.expected
-kdiff3 monkey.actual       monkey.expected       -o monkey.expected
-kdiff3 movement.actual     movement.expected     -o movement.expected
+kdiff3 catch_fish.expected   catch_fish.actual   -o catch_fish.expected
+kdiff3 compass.expected      compass.actual      -o compass.expected
+kdiff3 garden.expected       garden.actual       -o garden.expected
+kdiff3 monkey.expected       monkey.actual       -o monkey.expected
+kdiff3 movement.expected     movement.actual     -o movement.expected
