e35dc3c4c87e73a252dc079ad581408ec2e4b89e
Wesley Werner <wesley.werner@gmail.com>
2020-09-04 08:12:22 +1000

    Print release build stats.

--- tools/build_rel.bat
+++ tools/build_rel.bat
@@ -1,4 +1,4 @@
 @echo off
 @call setvars.bat
 mkdir ..\build
-inform6 +include_path=%I6PATH%\bin,%I6PATH%\contrib,..\src -~S -~D -~X ..\src\DrEgoAdv.inf ..\build\DrEgoAdv.z5
+inform6 +include_path=%I6PATH%\bin,%I6PATH%\contrib,..\src -~S -~D -~X -s ..\src\DrEgoAdv.inf ..\build\DrEgoAdv.z5
