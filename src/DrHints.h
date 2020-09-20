7d157a3be78029ae7d6ea944e734ccee0893c99f
Wesley Werner <wesley.werner@gmail.com>
2020-09-20 18:00:14 +1000

    Playtest changes: - examine craft, no such thing - put whip in bag, should wear it instead - new line before weather reports - ask rom about compass - jungle path description grammar - looking in direction without the compass should not happen - do not print multiple hints - reword garden description - examine garden, no such thing - rename vegetable plants to vegetables - grammar, go/come, give banana to monkey - monkey, take banana - examine gorge, no such thing - waterfall lead description, crossing the bridge I arrive at... - outer temple description typo, also reword the door's placement - put oil on stick, moves oil to the player - tie cloth to bone, should take them from bag first - dip torch in oil fails - light torch with matches, redirect to burn matches - don't put torch in bag when lit - let the torch burn out after twenty turns - allow the player to exit tomb when dark - enhance tomb idols descriptions - rename an oil substance - give drawing to Carver, (he) picks a piece... - when getting lost in jungle, drop any carried weighted object in last known location first - do not take Weighted object is already burdened - make rain drops more frequent - make rain start more often - reword, okay now lifting... - reword you spend the next week, to for the next five days you... - get in canoe win condition, also prints the action and contents of the canoe - play time, two hours, 300 turns

--- src/DrHints.h
+++ src/DrHints.h
@@ -18,73 +18,67 @@ with
     GoldEgg,
     ;
 
-[ HintPrint str;
-    Hints.Printed = true;
-    style roman;
-	style underline;
-	print (string) str; 
-	style roman;
-    print "^";
-];
-
 [ HintSub;
     Hints.Printed = false;
     if (Hints.Warning) {
         Hints.Warning = false;
-        HintPrint("You are about to ask Dr Hint for advice. Each time you type HINT
+        "You are about to ask Dr Hint for advice. Each time you type HINT
         you are given the next clue, based on your current situation, and each will be more of a spoiler.
-        If you are sure you want a clue, type HINT again.");
-        return;
+        If you are sure you want a clue, type HINT again.";
+    }
+    ! Exit a dark Tomb
+    if (location == thedark && real_location == Tomb) {
+        "You can feel your way to the stairs, and still go up. ";
     }
     ! Reclaim your compass from the monkey
     if (lodestar in monkey) {
-        style bold;
-        print "The monkey has your compass, ";
         if (banana notin player) {
             switch (++Hints.NoBanana) {
-                1: HintPrint("perhaps you could distract it with something else...");
-                2: HintPrint("maybe something edible will distract it...");
-                3: HintPrint("I hear they are fond of bananas...");
-                4: HintPrint("find the garden, near the bungalow...");
-                5: HintPrint("your whip may be useful to get the fruit...");
-                default: HintPrint("Whip the banana tree.");
+                1: "The monkey has your compass, it seems very interested in the
+                device, perhaps you could distract it with something else...";
+                2: "Something edible will distract the monkey...";
+                3: "Monkeys are fond of bananas...";
+                4: "Look in the garden near the bungalow...";
+                5: "Your whip may be useful to get the fruit down from the tree...";
+                default: "Whip the bananas.";
             }
         }
         if (banana in player) {
             switch (++Hints.HasBanana) {
-                1: HintPrint("give it a fruit, it may just forget about your compass.");
-                2: HintPrint("try leaving the fruit where the monkey can see it.");
-                default: HintPrint("DROP the banana on the ground, then WAIT.");
+                1: "Try give a fruit to the monkey...";
+                2: "If the monkey seems too skittish to go near you, try leaving
+                the fruit on the ground...";
+                default: "DROP the banana then WAIT.";
             }
         }
     }
     ! Seeing the way to the Valley
     if (Clearing has visited && Clearing.ne_to ~= valley_way) {
-        style bold;
-        print "To find the way, ";
         switch (++Hints.ValleySeen) {
-            1: HintPrint("perhaps the path is overgrown and not immediately visible...");
-            2: HintPrint("you might be able to see the path from a higher vantage point...");
-            3: HintPrint("you could try climbing up something...");
-            default: "climb up the fig tree and LOOK in all directions.";
+            1: "The path you seek may be overgrown, and not immediately visible...";
+            2: "You might be able to see the path from a higher vantage point...";
+            3: "Try climbing up something tall...";
+            4: "Climb the fig tree and LOOK in all directions...";
+            default: "The river is to the south, so you want to look for a path
+            in the opposite direction -- one of the three northern directions
+            NW, N or NE. ";
         }
     }
     ! Finding the alcove behind the waterfall
     if (Temple has visited && alcove_door notin Waterfall) {
-        style bold;
-        print "To find the way, ";
         if (temple_glyphs hasnt examined) {
             switch (++Hints.GlyphsNotSeen) {
-                1: HintPrint("examine all the details in the Temple...");
-                2: HintPrint("an ancient form of writing may give you a clue...");
-                default: HintPrint("examine the symbols engraved in the Temple walls.");
+                1: "Examine all the details in the Temple...";
+                2: "An ancient form of writing may give you a clue...";
+                default: "Examine the symbols engraved in the Temple walls...";
             }
         }
         if (temple_glyphs has examined) {
             switch (++Hints.GlyphsSeen) {
-                1: HintPrint("maybe your research notes can be of help...");
-                2: HintPrint("consult your journal on the glyph you see in the Temple...");
-                default: HintPrint("look behind the waterfall.");
+                1: "Your journal will be useful here...";
+                2: "Examine symbols in the Temple, then read notes on
+                the symbol you see...";
+                default: "Look behind the waterfall.";
             }
         }
     }
@@ -92,24 +86,24 @@ with
     if (alcove_door in Waterfall && Tomb hasnt visited) {
         if (GetPlayerTorchObject()) {
             switch (++Hints.HasTorch) {
-                1: HintPrint("oil makes good fuel for a wick...");
-                1: HintPrint("there is an urn of oil in the Temple...");
-                2: HintPrint("dip the torch in the oil...");
-                3: HintPrint("if your matches are wet, perhaps you could make sparks using a special stone...");
-                4: HintPrint("flint is a stone that throws sparks, they are commonly found in riverbeds or near waterfalls...");
-                5: HintPrint("flint has a distinctive blue glint....");
-                default: HintPrint("burn the torch with the flint.");
+                1: "Oil makes good fuel for a wick...";
+                1: "There is an urn in the Temple...";
+                2: "Dip the torch in the urn...";
+                3: "If your matches are wet, you could make sparks using a special stone...";
+                4: "Flint is a stone that throws sparks, and is commonly found in riverbeds or near waterfalls...";
+                5: "Flint has a distinctive blue glint....";
+                default: "Burn the torch with the flint.";
             }
         }
         else {
-            style bold;
-            print "You need a light, ";
             switch (++Hints.NoTorch) {
-                1: HintPrint("you could try make a torch...");
-                2: HintPrint("look for a stick or a bone, I am pretty sure you can find a stick in the jungle...");
-                3: HintPrint("you can tie some canvas to a stick, to make the wick...");
-                4: HintPrint("search the rubble in the bungalow...");
-                default: HintPrint("tie some cloth to the stick.");
+                1: "You need a light to enter the alcove, 
+                you could try make a torch...";
+                2: "Look for a stick or a bone...";
+                3: "You can tie some canvas to a stick, to make the wick...";
+                4: "Maybe you can find something useful in some rubble...";
+                5: "Search the rubble in the bungalow...";
+                default: "Tie the canvas to the stick.";
             }
         }
     }
@@ -119,59 +113,61 @@ with
         if (Carver.carving_state == 0) {
             if (sun_drawing notin player && sun_drawing notin satchel) {
                 switch (++Hints.KeyHole) {
-                    1: HintPrint("Examine that stone column in the Tomb...");
-                    2: HintPrint("Examine the hole next to the stone column in the Tomb...");
-                    default: HintPrint("Look inside the sun shaped hole in the Tomb.");
+                    1: "Examine that stone column in the Tomb...";
+                    2: "Examine the hole next to the stone column in the Tomb...";
+                    default: "Look inside the sun shaped hole in the Tomb.";
                 }
             }
             else if (box_trap hasnt moved) {
                 switch (++Hints.PlanBribe) {
-                    1: HintPrint("Did you visit the Tribe...");
-                    2: HintPrint("Did you notice the Carver in the Tribe...");
-                    3: HintPrint("You would think the Carver can make a replica, given the drawing...");
-                    4: HintPrint("The Carver wants a chicken as payment...");
-                    5: HintPrint("There must be a way to trap the chicken...");
-                    default: HintPrint("Go see Rom, your guide, for advice on catching chickens.");
+                    1: "Did you visit the Tribe...";
+                    2: "Did you notice the Carver in the Tribe...";
+                    3: "You would think the Carver can make a replica, given the drawing...";
+                    4: "The Carver wants a chicken as payment...";
+                    5: "There must be a way to trap the chicken...";
+                    default: "Go see Rom, your guide, for advice on catching chickens.";
                 }
             }
             else if (box_trap has moved) {
                 switch (++Hints.CatchChic) {
-                    1: HintPrint("Go where you saw a chicken...");
-                    2: HintPrint("There is a chicken in the Tribe...");
-                    3: HintPrint("Set the trap down on the ground first...");
-                    4: HintPrint("There should be seed in the trap, to lure the chicken in...");
-                    5: HintPrint("Remember to set the trap...");
-                    default: HintPrint("You need to pull the string, but only when the chicken is inside the trap.");
+                    1: "Go where you saw a chicken...";
+                    2: "There is a chicken in the Tribe...";
+                    3: "Set the trap down on the ground first...";
+                    4: "There should be seed in the trap, to lure the chicken in...";
+                    5: "Remember to set the trap...";
+                    default: "You need to pull the string, but only when the chicken is inside the trap.";
                 }
             }
         }
         ! Use the key to lower the altar
         if (sun_key in player || sun_key in satchel || sun_key in sun_key_hole) {
             switch (++Hints.LowerColumn) {
-                1: HintPrint("Now that you have the sun key, or a replica of it, it is time to use it...");
-                2: HintPrint("Usually one insert a key into a keyhole...");
-                3: HintPrint("It may take a few turns of the key...");
-                default: HintPrint("Insert the sun key in the key hole, and turn it.");
+                1: "Time to use the sun key...";
+                2: "Usually one inserts a key into a keyhole...";
+                3: "Did you notice the hole in the Tomb...";
+                default: "Insert the key in the hole and turn it.";
             }
         }
     }
     ! Taking the treasure
     if (stone_altar in Tomb && golden_egg in stone_altar) {
         switch (++Hints.GoldEgg) {
-            1: HintPrint("The treasure may be booby trapped...");
-            2: HintPrint("The treasure rests on a pressure sensitive surface...");
-            3: HintPrint("Before taking the treasure, put something heavy next to it, to prevent the pressure plate from activating...");
-            4: HintPrint("You may find something heavy in the Temple...");
-            default: HintPrint("The stone egg from the Temple should be placed on the altar, before taking the Golden Egg.");
+            1: "The treasure may be booby trapped...";
+            2: "The treasure rests on a pressure sensitive surface...";
+            3: "Before taking the treasure, put something heavy on the altar
+            to prevent the pressure plate from activating...";
+            4: "You may find something heavy in the Temple...";
+            default: "Put the stone egg on the altar, then take the golden egg.";
         }
     }
     ! Player has the prize
     if (golden_egg in player || golden_egg in satchel) {
-        HintPrint("It seems you have the treasure already, congrats! Guess you won't be needing that hint then. ");
+        "It seems you have the treasure already, congrats!
+        Guess you won't be needing that hint then. ";
     }
     ! No more hints
     if (~~Hints.Printed) {
-        HintPrint("I don't have any hints for you right now, try exploring a bit more.");
+        "I don't have any hints for you right now, try exploring a bit more. ";
     }
     !print "^";
 ];
